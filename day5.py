b306d79da037f922fe462818736f9445089fb20d
adam.trexler <adam.trexler@elderresearch.com>
2019-12-19 21:10:13 -0500

    kludge starter- passes one test lol

---
+++ day5.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 20:15:44 2019
+
+@author: adam.trexler
+"""
+
+def main():
+    with open("./data/day5.txt", "r") as f:
+        data = f.read()         
+        data = data.split(",")
+    
+    ix = 0 
+    def helper(data,howmany,ix,modes):
+        pos = {}
+        for x in range(howmany):
+            x += 1
+            pos[x] = int(data[data[ix+x]]) if modes[x] == 0 else int(data[ix+x])
+        return pos
+        
+    while ix < len(data):
+        print("opcode:",data[ix])
+        opcode = str(data[ix]).zfill(5) 
+        instruction = opcode[-2:] #get opcode last two digits
+        modes = {1:opcode[-3],
+                 2:opcode[-4],
+                 3:opcode[-5]}
+        
+        if instruction == "01":
+            pos = helper(data,3,ix,modes)
+            output = int(pos[1]) + int(pos[2])
+            data[pos[3]] = output
+            step = 4
+        elif instruction == "02":
+            pos = helper(data,3,ix,modes)
+            output = int(pos[1]) * int(pos[2])
+            data[pos[3]] = output
+            step = 4
+        elif instruction == "03":
+            # input instruction
+            pos = helper(data,1,ix,modes)
+            data[pos[1]] = 1
+            step = 2
+        elif instruction == "04":
+            # output instruction
+            pos = helper(data,1,ix,modes)
+            print(pos[1])
+            step = 2
+        elif instruction == "99":
+            print("end found")
+            break
+        else:
+            print("invalid opcode {x}".format(x=instruction))
+        ix += step
+            
+       
+if __name__ == '__main__':
+    main()
