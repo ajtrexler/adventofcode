cb0fecad54ee8b6278f7131d141e67d5f766ab74
adam.trexler <adam.trexler@elderresearch.com>
2019-12-27 15:09:53 -0500

    weird numpy reshaping effects- the silly way worked.

--- day8.py
+++ day8.py
@@ -7,20 +7,45 @@ day 8: advent of code
 
 import numpy as np
 from collections import Counter
+import matplotlib.pyplot as plt
 
-with open("./data/day8.txt", "r") as f:
-    data = f.read()         
-    data = data.replace("\n","")
-    data = [int(d) for d in data]
+def main():
+    with open("./data/day8.txt", "r") as f:
+        data = f.read()         
+        data = data.replace("\n","")
+        data = [int(d) for d in data]
+    
+    layers = []
+    zero_digits = []
+    for ix in range(0,len(data),150):
+        tmp = data[ix:ix+150]
+        layers.append(tmp)
+        zero_digits.append(sum([i==0 for i in tmp]))
+    
+    minzero_layer = layers[np.argmin(zero_digits)]
+    foo = Counter(minzero_layer)
+    
+    answer = foo[1]*foo[2]
+    print("oart A:",answer)
+    
+    # more complex with numpy array
+    # it is not clear why this did not work.  
+    # data = np.array(data).reshape(6,25,100)
+    # decoded = np.array([(data[i,k,:][data[i,k,:] != 2])[0] for k in range(25) for i in range(6)]).reshape(6,25)
+    
+    vals = []    
+    for ix in range(150):
+        s = [data[i+ix] for i in range(0,len(data),150)]
+        foo = (np.array(s)[[ss!=2 for ss in s]])[0]
+        vals.append(foo)
 
-layers = []
-zero_digits = []
-for ix in range(0,len(data),150):
-    tmp = data[ix:ix+150]
-    layers.append(tmp)
-    zero_digits.append(sum([i==0 for i in tmp]))
+    decode2 = np.array(vals).reshape(6,25)
+    np.set_printoptions(formatter={'all':lambda x: ' ' if x==0 else 'x'})
+    print("############\n")
+    print(decode2)
+    #plt.imshow(decode2)
 
-minzero_layer = layers[np.argmin(zero_digits)]
-foo = Counter(minzero_layer)
+if __name__ == '__main__':
+    main()
 
-answer = foo[1]*foo[2]
+
